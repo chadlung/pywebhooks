e5d0afaddc033260278b9e66a6f08d54d20fc9df
chadlung <chad.lung@gmail.com>
2014-01-25 16:00:57 -0600

    Remove un-used imports, PEP8 fixes

--- pywebhooks/app.py
+++ pywebhooks/app.py
@@ -4,9 +4,6 @@ from multiprocessing import Process
 from flask import Flask
 from flask import abort, request, jsonify, g, url_for
 from flask.ext.httpauth import HTTPBasicAuth
-#from flask.ext.sqlalchemy import SQLAlchemy
-
-from passlib.apps import custom_app_context as pwd_context
 
 from pywebhooks.models.user import User
 from pywebhooks.models.user import db
@@ -60,9 +57,9 @@ def new_user():
     email = request.json.get('email')
     password = request.json.get('password')
     if username is None or password is None or email is None:
-        abort(400) # missing arguments
+        abort(400)  # missing arguments
     if User.query.filter_by(username=username).first() is not None:
-        abort(400) # existing user
+        abort(400)  # existing user
     user = User(username=username, email=email)
     user.hash_password(password)
     db.session.add(user)
