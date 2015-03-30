49812fb375c3382c833d1733284f2092c8f684b1
chadlung <chad.lung@gmail.com>
2015-03-29 22:15:42 -0500

    Update project files

--- README.rst
+++ README.rst
@@ -2,4 +2,3 @@ PyWebHooks
 ==========
 
 WebHooks as a Service (work in progress, nothing to see here yet)
-^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
+"""
+PyWebHooks Setup
+"""
+
 try:
     from setuptools import setup, find_packages
 except ImportError:
@@ -7,20 +11,29 @@ except ImportError:
 
 
 def read(relative):
+    """
+    Read file contents and return a list of lines.
+    ie, read the VERSION file
+    """
     contents = open(relative, 'r').read()
     return [l for l in contents.split('\n') if l != '']
 
+with open('README.rst', 'r') as f:
+    readme = f.read()
 
 setup(
-    name='pywebhooks',
+    name='viperpy',
+    url='https://github.com/chadlung/pywenhooks',
+    keywords=['WebHooks'],
+    long_description=readme,
     version=read('VERSION')[0],
-    description='PyWebHooks - WebHooks as a Service',
+    description='Python WebHooks',
     author='Chad Lung',
     author_email='chad.lung@gmail.com',
-    tests_require=read('test-requirements.txt'),
-    install_requires=read('requirements.txt'),
+    tests_require=read('./test-requirements.txt'),
+    install_requires=read('./requirements.txt'),
     test_suite='nose.collector',
     zip_safe=False,
     include_package_data=True,
-    packages=find_packages(exclude=['ez_setup', 'tests'])
+    packages=find_packages(exclude=['ez_setup'])
 )
