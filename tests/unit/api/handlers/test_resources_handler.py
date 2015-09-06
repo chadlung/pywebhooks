b703cb6e9cbc47335c94009ab7b51613e016fe81
chadlung <chad.lung@gmail.com>
2015-09-06 16:21:20 -0500

    Add more unit tests

--- tests/unit/api/handlers/test_resources_handler.py
+++ tests/unit/api/handlers/test_resources_handler.py
@@ -4,7 +4,7 @@ import unittest
 from unittest.mock import patch
 
 # Third party imports
-# None
+from rethinkdb.errors import RqlRuntimeError, RqlDriverError
 
 # Project level imports
 from pywebhooks.app import create_wsgi_app
@@ -13,7 +13,8 @@ from pywebhooks import DEFAULT_REGISTRATIONS_TABLE, \
 from pywebhooks.database.rethinkdb.interactions import Interactions
 from pywebhooks.api.handlers.resources_handler import \
     registration_id_exists, lookup_subscription_id, lookup_registration_id, \
-    lookup_account_id, validate_access
+    lookup_account_id, validate_access, update, query, delete_all, \
+    delete_accounts_except_admins, delete_registration, delete, insert
 
 
 def suite():
@@ -27,6 +28,11 @@ class WhenTestingResourcesHandler(unittest.TestCase):
     def setUp(self):
         self.app = create_wsgi_app()
         self.app.config['TESTING'] = True
+
+        self.param_kwargs = {
+            'tes1': 1,
+            'test2': 2
+        }
 
     def test_registration_id_exists(self):
         with patch.object(Interactions, 'query', return_value=True) as \
@@ -161,3 +167,229 @@ class WhenTestingResourcesHandler(unittest.TestCase):
             response = validate_access(
                 'fred', incoming_account_id='111222')
             self.assertIsNone(response)
+
+    def test_update_bad_request(self):
+        with self.app.test_request_context():
+            with patch.object(Interactions, 'update', return_value=None):
+                response = update(DEFAULT_REGISTRATIONS_TABLE,
+                                  record_id='123',
+                                  username=None,
+                                  updates={})
+                self.assertEqual(response.status_code, client.BAD_REQUEST)
+
+    def test_update_record_id(self):
+        with self.app.test_request_context():
+            with patch.object(Interactions, 'update', return_value={}):
+                response = update(DEFAULT_REGISTRATIONS_TABLE,
+                                  record_id='123',
+                                  username=None,
+                                  updates={})
+                self.assertEqual(response.status_code, client.OK)
+
+    def test_update_rql_runtime_error(self):
+        with self.app.test_request_context():
+            with patch.object(Interactions, 'update',
+                              side_effect=RqlRuntimeError(None, None, None)):
+                response = update(DEFAULT_REGISTRATIONS_TABLE,
+                                  record_id='123', username=None, updates={})
+                self.assertRaises(RqlRuntimeError)
+                self.assertEqual(response.status_code,
+                                 client.INTERNAL_SERVER_ERROR)
+
+    def test_update_rql_driver_error(self):
+        with self.app.test_request_context():
+            with patch.object(Interactions, 'update',
+                              side_effect=RqlDriverError(None)):
+                update(DEFAULT_REGISTRATIONS_TABLE,
+                       record_id='123',
+                       username=None,
+                       updates={})
+                self.assertRaises(RqlDriverError)
+
+    def test_update_no_record_id(self):
+        with self.app.test_request_context():
+            with patch.object(Interactions, 'update', return_value={}):
+                response = update(DEFAULT_REGISTRATIONS_TABLE,
+                                  record_id=None,
+                                  username='johndoe',
+                                  updates={})
+                self.assertEqual(response.status_code, client.OK)
+
+    def test_query(self):
+        with self.app.test_request_context():
+            with patch.object(Interactions, 'get', return_value={}):
+                response = query(DEFAULT_REGISTRATIONS_TABLE, record_id='123')
+                self.assertEqual(response.status_code, client.OK)
+
+    def test_query_rql_runtime_error(self):
+        with self.app.test_request_context():
+            with patch.object(Interactions, 'get',
+                              side_effect=RqlRuntimeError(None, None, None)):
+                response = query(DEFAULT_REGISTRATIONS_TABLE, record_id='123')
+                self.assertRaises(RqlRuntimeError)
+                self.assertEqual(response.status_code,
+                                 client.INTERNAL_SERVER_ERROR)
+
+    def test_query_rql_driver_error(self):
+        with self.app.test_request_context():
+            with patch.object(Interactions, 'get',
+                              side_effect=RqlDriverError(None)):
+                response = query(DEFAULT_REGISTRATIONS_TABLE, record_id='123')
+                self.assertRaises(RqlDriverError)
+                self.assertEqual(response.status_code,
+                                 client.INTERNAL_SERVER_ERROR)
+
+    def test_delete_all(self):
+        with self.app.test_request_context():
+            with patch.object(Interactions, 'delete_all', return_value={}):
+                response = delete_all(DEFAULT_REGISTRATIONS_TABLE)
+                self.assertEqual(response.status_code, client.OK)
+
+    def test_delete_all_rql_runtime_error(self):
+        with self.app.test_request_context():
+            with patch.object(Interactions, 'delete_all',
+                              side_effect=RqlRuntimeError(None, None, None)):
+                response = delete_all(DEFAULT_REGISTRATIONS_TABLE)
+                self.assertRaises(RqlRuntimeError)
+                self.assertEqual(response.status_code,
+                                 client.INTERNAL_SERVER_ERROR)
+
+    def test_delete_all_rql_driver_error(self):
+        with self.app.test_request_context():
+            with patch.object(Interactions, 'delete_all',
+                              side_effect=RqlDriverError(None)):
+                response = delete_all(DEFAULT_REGISTRATIONS_TABLE)
+                self.assertRaises(RqlDriverError)
+                self.assertEqual(response.status_code,
+                                 client.INTERNAL_SERVER_ERROR)
+
+    def test_delete_accounts_except_admins(self):
+        with self.app.test_request_context():
+            with patch.object(Interactions, 'delete_specific',
+                              return_value={}):
+                response = delete_accounts_except_admins()
+                self.assertEqual(response.status_code, client.OK)
+
+    def test_delete_accounts_except_admins_rql_runtime_error(self):
+        with self.app.test_request_context():
+            with patch.object(Interactions, 'delete_specific',
+                              side_effect=RqlRuntimeError(None, None, None)):
+                response = delete_accounts_except_admins()
+                self.assertRaises(RqlRuntimeError)
+                self.assertEqual(response.status_code,
+                                 client.INTERNAL_SERVER_ERROR)
+
+    def test_delete_accounts_except_admins_rql_driver_error(self):
+        with self.app.test_request_context():
+            with patch.object(Interactions, 'delete_specific',
+                              side_effect=RqlDriverError(None)):
+                response = delete_accounts_except_admins()
+                self.assertRaises(RqlDriverError)
+                self.assertEqual(response.status_code,
+                                 client.INTERNAL_SERVER_ERROR)
+
+    def test_delete_registration(self):
+        with self.app.test_request_context():
+            with patch.object(Interactions, 'delete_specific',
+                              return_value={}):
+                response = delete_registration('123')
+                self.assertEqual(response.status_code, client.OK)
+
+    def test_delete_registration_rql_runtime_error(self):
+        with self.app.test_request_context():
+            with patch.object(Interactions, 'delete_specific',
+                              side_effect=RqlRuntimeError(None, None, None)):
+                response = delete_registration('123')
+                self.assertRaises(RqlRuntimeError)
+                self.assertEqual(response.status_code,
+                                 client.INTERNAL_SERVER_ERROR)
+
+    def test_delete_registration_rql_driver_error(self):
+        with self.app.test_request_context():
+            with patch.object(Interactions, 'delete_specific',
+                              side_effect=RqlDriverError(None)):
+                response = delete_registration('123')
+                self.assertRaises(RqlDriverError)
+                self.assertEqual(response.status_code,
+                                 client.INTERNAL_SERVER_ERROR)
+
+    def test_delete_registration_type_error(self):
+        with self.app.test_request_context():
+            with patch.object(Interactions, 'delete_specific',
+                              side_effect=TypeError):
+                response = delete_registration('123')
+                self.assertRaises(TypeError)
+                self.assertEqual(response.status_code,
+                                 client.BAD_REQUEST)
+
+    def test_delete(self):
+        with self.app.test_request_context():
+            with patch.object(Interactions, 'delete',
+                              return_value={}):
+                response = delete(DEFAULT_SUBSCRIPTIONS_TABLE, '123')
+                self.assertEqual(response.status_code, client.OK)
+
+    def test_delete_rql_runtime_error(self):
+        with self.app.test_request_context():
+            with patch.object(Interactions, 'delete',
+                              side_effect=RqlRuntimeError(None, None, None)):
+                response = delete(DEFAULT_SUBSCRIPTIONS_TABLE, '123')
+                self.assertRaises(RqlRuntimeError)
+                self.assertEqual(response.status_code,
+                                 client.INTERNAL_SERVER_ERROR)
+
+    def test_delete_rql_driver_error(self):
+        with self.app.test_request_context():
+            with patch.object(Interactions, 'delete',
+                              side_effect=RqlDriverError(None)):
+                response = delete(DEFAULT_SUBSCRIPTIONS_TABLE, '123')
+                self.assertRaises(RqlDriverError)
+                self.assertEqual(response.status_code,
+                                 client.INTERNAL_SERVER_ERROR)
+
+    def test_delete_type_error(self):
+        with self.app.test_request_context():
+            with patch.object(Interactions, 'delete',
+                              side_effect=TypeError):
+                response = delete(DEFAULT_SUBSCRIPTIONS_TABLE, '123')
+                self.assertRaises(TypeError)
+                self.assertEqual(response.status_code,
+                                 client.BAD_REQUEST)
+
+    def test_insert(self):
+        with self.app.test_request_context():
+            with patch.object(Interactions, 'insert',
+                              return_value={}):
+                response = insert(DEFAULT_SUBSCRIPTIONS_TABLE,
+                                  **self.param_kwargs)
+                self.assertEqual(response.status_code, client.CREATED)
+
+    def test_insert_rql_runtime_error(self):
+        with self.app.test_request_context():
+            with patch.object(Interactions, 'insert',
+                              side_effect=RqlRuntimeError(None, None, None)):
+                response = insert(DEFAULT_SUBSCRIPTIONS_TABLE,
+                                  **self.param_kwargs)
+                self.assertRaises(RqlRuntimeError)
+                self.assertEqual(response.status_code,
+                                 client.INTERNAL_SERVER_ERROR)
+
+    def test_insert_rql_driver_error(self):
+        with self.app.test_request_context():
+            with patch.object(Interactions, 'insert',
+                              side_effect=RqlDriverError(None)):
+                response = insert(DEFAULT_SUBSCRIPTIONS_TABLE,
+                                  **self.param_kwargs)
+                self.assertRaises(RqlDriverError)
+                self.assertEqual(response.status_code,
+                                 client.INTERNAL_SERVER_ERROR)
+
+    def test_insert_type_error(self):
+        with self.app.test_request_context():
+            with patch.object(Interactions, 'insert',
+                              side_effect=TypeError):
+                response = insert(DEFAULT_SUBSCRIPTIONS_TABLE,
+                                  **self.param_kwargs)
+                self.assertRaises(TypeError)
+                self.assertEqual(response.status_code,
+                                 client.BAD_REQUEST)
